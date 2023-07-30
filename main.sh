5e01126feb1309caad9e6d26a2e7beabac11f994
Prianger <mail@maximilianbeyer.de>
2023-07-30 02:27:38 +0200

    .

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Updates werden installiert"
-sudo apt update
-sudo apt upgrade -y
-sudo apt autoremove -y
+#sudo apt update
+#sudo apt upgrade -y
+#sudo apt autoremove -y
 
 echo "Docker werden installiert?"
 apt install docker.io docker-compose -y
@@ -11,20 +11,22 @@ apt install docker.io docker-compose -y
 echo "Zusatzpakete werden installiert"
 apt install wget unzip git -y
 
-#echo "Ordner erstellen"
-#mkdir /invoiceninja
-#cd /invoiceninja
-
 echo "Git Repo clonen"
 git clone https://github.com/invoiceninja/dockerfiles.git
 cd dockerfiles
 
-echo "APP_KEY generieren"
-docker run --rm -it invoiceninja/invoiceninja php artisan key:generate --show > appkey.txt
+# Schlüssel generieren
+echo "APP_Key generieren"
+key=$(docker run --rm -it invoiceninja/invoiceninja php artisan key:generate --show)
+
+# Schlüssel in die Datei einfügen
+sed -i "s|APP_KEY=<insert your generated key in here>|APP_KEY=$key|" ~/invoiceninja/dockerfiles/env
+
 
 echo "Ordner Berechtigung anpassen"
 chmod 755 docker/app/public
 sudo chown -R 1500:1500 docker/app
 
 nano env
+
  
